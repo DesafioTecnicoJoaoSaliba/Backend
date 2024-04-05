d915c0c47164afd683f7d659d68aa539e0484bc2
Joao Gabriel Saliba Ribeiro <joao_salibaa@hotmail.com>
2024-04-05 13:44:38 -0300

    feat: Using Response Entity

--- src/main/java/desafiotecnicojr/desafiotecnico/controller/ProdutoController.java
+++ src/main/java/desafiotecnicojr/desafiotecnico/controller/ProdutoController.java
@@ -10,6 +10,8 @@ import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,13 +26,14 @@ public class ProdutoController {
 
     @Operation(summary = "Cria um novo produto")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Produto criado com sucesso"),
+            @ApiResponse(responseCode = "201", description = "Produto criado com sucesso"),
             @ApiResponse(responseCode = "400", description = "Requisição inválida"),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     })
     @PostMapping
-    public ProdutoOutput createProduct(@RequestBody @Valid ProdutoDTO produtoDTO) {
-        return this.produtoService.createProduct(produtoDTO);
+    public ResponseEntity<ProdutoOutput> createProduct(@RequestBody @Valid ProdutoDTO produtoDTO) {
+        ProdutoOutput produtoOutput = this.produtoService.createProduct(produtoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoOutput);
     }
 
     @Operation(summary = "Retorna um produto por ID")
@@ -38,20 +41,22 @@ public class ProdutoController {
             @ApiResponse(responseCode = "200", description = "Produto retornado com sucesso"),
             @ApiResponse(responseCode = "404", description = "Produto não encontrado")
     })
-    @GetMapping("{id}")
-    public ProdutoDTO getProduct(@PathVariable Long id) {
-        return this.produtoService.getProduct(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> getProduct(@PathVariable Long id) {
+        ProdutoDTO produtoDTO = this.produtoService.getProduct(id);
+        return ResponseEntity.ok().body(produtoDTO);
     }
 
-    @Operation(summary = "Retorna todos os produtos ou filtra por nome e/ou descrição")
+    @Operation(summary = "Retorna todos os produtos ou filtra por nome e descrição")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Lista de produtos ou produtos filtrados"),
             @ApiResponse(responseCode = "400", description = "Requisição inválida"),
             @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     })
     @GetMapping()
-    public Page<ProdutoOutput> findProdutoDTO(@ParameterObject Pageable pageable, @RequestParam(required = false) String nome, @RequestParam(required = false) String descricao) {
-        return this.produtoService.findProdutoDTO(pageable, nome, descricao);
+    public ResponseEntity<Page<ProdutoOutput>> findProdutoDTO(@ParameterObject Pageable pageable, @RequestParam(required = false) String nome, @RequestParam(required = false) String descricao) {
+        Page<ProdutoOutput> produtos = this.produtoService.findProdutoDTO(pageable, nome, descricao);
+        return ResponseEntity.ok().body(produtos);
     }
 
     @Operation(summary = "Exclui um produto por ID")
@@ -59,8 +64,9 @@ public class ProdutoController {
             @ApiResponse(responseCode = "204", description = "Produto excluído com sucesso"),
             @ApiResponse(responseCode = "404", description = "Produto não encontrado")
     })
-    @DeleteMapping("{id}")
-    public void deleteProduct(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         this.produtoService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/test/java/desafiotecnicojr/desafiotecnico/ProdutoControllerTest.java
+++ src/test/java/desafiotecnicojr/desafiotecnico/ProdutoControllerTest.java
@@ -12,6 +12,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -19,7 +21,6 @@ import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 class ProdutoControllerTest {
 
@@ -35,41 +36,45 @@ class ProdutoControllerTest {
         ProdutoOutput produtoOutput = new ProdutoOutput(1L, "Produto Teste", "Descrição do Produto Teste", BigDecimal.TEN, LocalDateTime.now());
         when(produtoService.createProduct(produtoDTO)).thenReturn(produtoOutput);
 
-        ProdutoDTO responseEntity = produtoController.createProduct(produtoDTO);
+        ResponseEntity<ProdutoOutput> responseEntity = produtoController.createProduct(produtoDTO);
 
-        assertEquals(produtoDTO, responseEntity);
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(produtoOutput, responseEntity.getBody());
     }
 
     @Test
     void getProduct() {
         Long productId = 1L;
         ProdutoDTO produtoDTO = new ProdutoDTO(1L, "Produto Teste", "Descrição do Produto Teste", BigDecimal.TEN);
-        ProdutoOutput produtoOutput = new ProdutoOutput(1L, "Produto Teste", "Descrição do Produto Teste", BigDecimal.TEN, LocalDateTime.now());
-        when(produtoService.createProduct(produtoDTO)).thenReturn(produtoOutput);
+        when(produtoService.getProduct(productId)).thenReturn(produtoDTO);
 
-        ProdutoDTO responseEntity = produtoController.getProduct(productId);
+        ResponseEntity<ProdutoDTO> responseEntity = produtoController.getProduct(productId);
 
-        assertEquals(produtoDTO, responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(produtoDTO, responseEntity.getBody());
     }
 
     @Test
     void findProdutoDTO() {
         Pageable pageable = Pageable.unpaged();
-        ProdutoOutput produtoDTO = new ProdutoOutput(1L, "Produto Teste", "Descrição do Produto Teste", BigDecimal.TEN,LocalDateTime.now());
+        ProdutoOutput produtoDTO = new ProdutoOutput(1L, "Produto Teste", "Descrição do Produto Teste", BigDecimal.TEN, LocalDateTime.now());
         Page<ProdutoOutput> produtoPage = new PageImpl<>(Collections.singletonList(produtoDTO));
         when(produtoService.findProdutoDTO(pageable, null, null)).thenReturn(produtoPage);
 
-        Page<ProdutoOutput> responseEntity =  produtoController.findProdutoDTO(pageable, null, null);
+        ResponseEntity<Page<ProdutoOutput>> responseEntity = produtoController.findProdutoDTO(pageable, null, null);
 
-        assertEquals(produtoPage, responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(produtoPage, responseEntity.getBody());
     }
 
     @Test
     void deleteProduct() {
         Long productId = 1L;
 
-        produtoController.deleteProduct(productId);
+        ResponseEntity<Void> responseEntity = produtoController.deleteProduct(productId);
 
+        assertEquals(HttpStatus.NO_CONTENT, responseEntity.getStatusCode());
         verify(produtoService, times(1)).deleteProduct(productId);
     }
 }
+
